c2465955fb6dd8f5496f2913111c217326ece6d9
Chris Norman <chris@norman.me>
2013-03-27 15:44:45 +0000

    Attempting fix for handlebars recurring templates issue

--- lib/sht_rails/tilt.rb
+++ lib/sht_rails/tilt.rb
@@ -14,16 +14,19 @@ module ShtRails
 
     def evaluate(scope, locals, &block)
       template_key = path_to_key scope
+      
+      js_key = template_key.inspect.gsub('/', '.')
+      
       <<-HandlebarsTemplate
   (function() { 
   #{namespace} || (#{namespace} = {});
   #{namespace}CachedShtTemplates || (#{namespace}CachedShtTemplates = {});
-  #{namespace}CachedShtTemplates[#{template_key.inspect}] = Handlebars.compile(#{data.inspect});
-  #{namespace}[#{template_key.inspect}] = function(object) {
+  #{namespace}CachedShtTemplates[#{js_key}] = Handlebars.compile(#{data.inspect});
+  #{namespace}[#{js_key}] = function(object) {
     if (object == null){
-      return #{ShtRails.template_namespace}CachedShtTemplates[#{template_key.inspect}];
+      return #{ShtRails.template_namespace}CachedShtTemplates[#{js_key}];
     } else {
-      return #{ShtRails.template_namespace}CachedShtTemplates[#{template_key.inspect}](object);
+      return #{ShtRails.template_namespace}CachedShtTemplates[#{js_key}](object);
     }
   };
   }).call(this);
@@ -33,7 +36,7 @@ module ShtRails
     def path_to_key(scope)
       path = scope.logical_path.to_s.split('/')
       path.last.gsub!(/^_/, '')
-      path.join('.')
+      path.join('/')
     end
   end
 end
