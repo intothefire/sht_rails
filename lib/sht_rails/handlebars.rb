04a6c5e7af2fe37f0aadc5c9dcc48302cbbfd6d6
Chris Norman <chris@norman.me>
2013-05-31 16:53:56 +0100

    Added ability to include helpers in handlebars templates

--- lib/sht_rails/handlebars.rb
+++ lib/sht_rails/handlebars.rb
@@ -13,7 +13,13 @@ module ShtRails
   end if defined?(partials) && partials.is_a?(Hash)
   
   helpers.each do |key, value|
-    hbs_context_for_sht.register_helper(key, value)
+    hbs_context_for_sht.register_helper(key) do |context, condition, block|
+      if condition
+        "#{block.fn(context)}#{block.fn(context)}"
+      else
+        block.inverse(context)
+      end
+    end
   end if defined?(helpers) && partials.is_a?(Hash)
   
   hbs_context_for_sht.compile(#{template.source.inspect}).call(#{ShtRails.action_view_key.to_s} || {}).html_safe
