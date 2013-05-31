bf26d9be18c279683f87c90369529a37cdb06db4
Chris Norman <chris@norman.me>
2013-05-31 17:39:51 +0100

    Revert "Removed ability to include helpers in handlebars templates"
    This reverts commit 4706a47b65a45e6436005c0bcffef7ba19f7f1a5.

--- lib/sht_rails/handlebars.rb
+++ lib/sht_rails/handlebars.rb
@@ -11,6 +11,7 @@ module ShtRails
   partials.each do |key, value|
     hbs_context_for_sht.register_partial(key, value)
   end if defined?(partials) && partials.is_a?(Hash)
+
   hbs_context_for_sht.compile(#{template.source.inspect}).call(#{ShtRails.action_view_key.to_s} || {}).html_safe
 SHT
       else
